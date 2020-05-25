0c81a911226a4035bcb85869aadbee3667237839
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-05-25 17:55:18 +0200

    Update next-i18next, get rid of custom server

--- next.config.js
+++ next.config.js
@@ -1,23 +1,36 @@
+const { nextI18NextRewrites } = require('next-i18next/rewrites');
+
 const config = require('./config');
 
+const localeSubpaths = Object.keys(config.search.languages).reduce((langs, l) => {
+  langs[l] = l;
+  return langs;
+}, {});
+
 module.exports = {
+  publicRuntimeConfig: {
+    localeSubpaths,
+  },
   experimental: {
     async rewrites() {
-      return Object.entries(config.routes).flatMap(([routeName, route]) => {
-        const rewrites = [
-          {
-            source: `/${routeName}{/}?`,
-            destination: `/${route.view}?type=${routeName}`,
-          },
-        ];
-        if (route.details) {
-          rewrites.push({
-            source: `/${routeName}/:id`,
-            destination: `/details/${route.details.view}?id=:id&type=${routeName}`,
-          });
-        }
-        return rewrites;
-      });
+      return [
+        ...nextI18NextRewrites(localeSubpaths),
+        ...Object.entries(config.routes).flatMap(([routeName, route]) => {
+          const rewrites = [
+            {
+              source: `/${routeName}{/}?`,
+              destination: `/${route.view}?type=${routeName}`,
+            },
+          ];
+          if (route.details) {
+            rewrites.push({
+              source: `/${routeName}/:id`,
+              destination: `/details/${route.details.view}?id=:id&type=${routeName}`,
+            });
+          }
+          return rewrites;
+        }),
+      ];
     },
   },
 };
