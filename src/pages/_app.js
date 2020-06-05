743a5bf8043b49755ef51c0f71efef906358526b
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-06-05 15:10:25 +0200

    Fix warning with meta charset attribute

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,7 +20,7 @@ class MyApp extends App {
         <ReakitProvider>
           <Reset />
           <Head>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             {/* Import Lato font */}
             <link
