a1ed5746b5a894e47fa01c938ebbdc28b8790573
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-06-22 14:52:53 +0200

    Update Video page, add react-player, add limecraft connector

--- src/pages/details/video.js
+++ src/pages/details/video.js
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
+import DefaultErrorPage from 'next/error';
+import NextAuth from 'next-auth/client';
+import ReactPlayer from 'react-player';
 
 import { Header, Footer, Layout, Body, Media } from '@components';
 import Metadata from '@components/Metadata';
 import Tabs, { Tab } from '@components/TabBar';
 import PageTitle from '@components/PageTitle';
+import Debug from '@components/Debug';
 import { breakpoints } from '@styles';
-import { uriToId, idToUri } from '@helpers/utils';
-import config from '~/config';
+import { idToUri, uriToId } from '@helpers/utils';
+import SparqlClient from '@helpers/sparql';
+import { locatorToVideo } from '@helpers/connectors/limecraft';
 
-const sparqlTransformer = require('sparql-transformer').default;
+import { withTranslation } from '~/i18n';
+import config from '~/config';
 
 const Columns = styled.div`
   display: flex;
@@ -24,33 +30,85 @@ const Columns = styled.div`
 `;
 
 const Primary = styled.div`
-  flex: 1;
+  flex: auto;
+  overflow: hidden;
+  min-width: 50%;
   padding-right: 24px;
   padding-top: 24px;
   margin-left: 24px;
 
   display: flex;
   flex-direction: column;
+
+  .carousel .thumbs {
+    /* TODO: HACK: react-responsive-carousel doesn't support vertical thumbnails as of 2020-04-27 */
+    white-space: normal;
+    transform: none !important;
+  }
+  .carousel .thumb.selected,
+  .carousel .thumb:hover {
+    border: 3px solid ${({ theme }) => theme.colors.primary};
+  }
+  .carousel .slide {
+    background: #d9d9d9;
+
+    img {
+      width: auto;
+      max-width: 100%;
+    }
+  }
+  .carousel .carousel-status {
+    font-size: 16px;
+    color: #d4d3ce;
+    top: 16px;
+    left: 16px;
+  }
+  .carousel .control-arrow::before {
+    border-width: 0 3px 3px 0;
+    border: solid #000;
+    display: inline-block;
+    padding: 3px;
+    border-width: 0 3px 3px 0;
+    width: 20px;
+    height: 20px;
+  }
+  .carousel .control-next.control-arrow::before {
+    transform: rotate(-45deg);
+  }
+  .carousel .control-prev.control-arrow::before {
+    transform: rotate(135deg);
+  }
 `;
 
 const Secondary = styled.div`
-  flex: 1;
+  flex: auto;
   padding-top: 24px;
   margin-left: 24px;
 
   ${breakpoints.desktop`
     padding-right: 24px;
     margin-left: 0;
-    max-width: 30%;
   `}
 `;
 
-const Player = styled.div`
-  /* background-color: green; */
+const Title = styled.h1`
+  display: none;
+  border-bottom: 3px solid ${({ theme }) => theme.colors.primary};
+  font-size: 48px;
+  line-height: 1.25;
+  margin-bottom: 24px;
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  ${breakpoints.desktop`
+    display: block;
+  `}
+`;
+
+const MobileTitle = styled(Title)`
+  display: block;
+
+  ${breakpoints.desktop`
+    display: none;
+  `};
 `;
 
 const Analysis = styled.div`
@@ -63,24 +121,81 @@ const MetadataList = styled.div`
   margin-bottom: 24px;
 `;
 
-const RelatedVideos = styled.div`
-  background-color: #eee;
-  padding: 16px;
-  border: 1px solid #dcdcdc;
-
-  h2 {
-    text-transform: uppercase;
-    font-weight: bold;
-    color: ${({ theme }) => theme.colors.primary};
-    margin-bottom: 1em;
-  }
-`;
-
 const RelatedVideosList = styled.div``;
 
-const VideoDetailsPage = ({ result }) => {
+function generateValue(currentRouteName, currentRoute, metaName, meta) {
+  if (typeof meta === 'string') {
+    return <>{meta}</>;
+  }
+
+  const [routeName, route] =
+    Object.entries(config.routes).find(([, r]) => {
+      return r.rdfType && r.rdfType === meta['@type'];
+    }) || [];
+  const isKnownType = typeof type !== 'undefined';
+
+  let url = meta['@id'];
+  if (route) {
+    url = `/${routeName}/${uriToId(meta['@id'], { encoding: !route.uriBase })}`;
+  } else if (
+    currentRoute &&
+    Array.isArray(currentRoute.filters) &&
+    currentRoute.filters.find((f) => f.id === metaName)
+  ) {
+    url = `/${currentRouteName}/?field_filter_${metaName}=${encodeURIComponent(meta['@id'])}`;
+  }
+
+  let printableValue = '<unk>';
+  if (typeof meta.label === 'object') {
+    // If $langTag is set to 'show' in sparql-transformer
+    printableValue = meta.label['@value'];
+  } else if (typeof meta.label === 'string') {
+    // Example: {"@id":"http://data.silknow.org/collection/ec0f9a6f-7b69-31c4-80a6-c0a9cde663a5","@type":"http://erlangen-crm.org/current/E78_Collection","label":"European Sculpture and Decorative Arts"}
+    printableValue = meta.label;
+  } else {
+    // Example: {"@id":"Textiles"}
+    printableValue = meta['@id'];
+    url = null;
+  }
+
+  if (!url) {
+    return <>{printableValue}</>;
+  }
+
+  return (
+    <a
+      href={url}
+      target={isKnownType ? '_self' : '_blank'}
+      rel={isKnownType ? '' : 'noopener noreferrer'}
+    >
+      {printableValue}
+    </a>
+  );
+}
+
+const VideoDetailsPage = ({ result, mediaUrl, t }) => {
+  if (!result) {
+    return <DefaultErrorPage statusCode={404} title="Result not found" />;
+  }
+
   const { query } = useRouter();
+  const [session] = NextAuth.useSession();
   const route = config.routes[query.type];
+
+  const images = [];
+  const representations = Array.isArray(result.representation)
+    ? result.representation
+    : [result.representation].filter((x) => x);
+  representations.forEach((repres) => {
+    const imgs = Array.isArray(repres.image) ? repres.image : [repres.image];
+    images.push(...imgs.filter((img) => img && new URL(img).hostname === 'silknow.org'));
+  });
+
+  const metadata = Object.entries(result).filter(([metaName, meta]) => {
+    return !['@type', '@id', '@graph', 'label', 'representation', 'mediaLocator'].includes(
+      metaName
+    );
+  });
 
   const label = route.labelFunc(result);
 
@@ -89,12 +204,48 @@ const VideoDetailsPage = ({ result }) => {
       <PageTitle title={`${label}`} />
       <Header />
       <Body>
+        <ReactPlayer url={mediaUrl} width={null} height="50vh" controls={true} playing={true} />
         <Columns>
           <Primary>
-            <Player>
-              <img src="/images/thumbnail.jpg" alt="Video" />
-            </Player>
-            <Analysis>
+            <Title>{label}</Title>
+            <MetadataList>
+              {metadata.flatMap(([metaName, meta]) => {
+                const values = [];
+                if (Array.isArray(meta)) {
+                  /* Example:
+                    [
+                      { '@id': { '@language': 'en', '@value': 'linen' } },
+                      { '@id': 'http://data.silknow.org/vocabulary/277', label: [ { '@language': 'en', '@value': 'silk thread' } ] }
+                    ]
+                  */
+                  meta.forEach((subMeta) => {
+                    const value = generateValue(query.type, route, metaName, subMeta);
+                    if (value) {
+                      values.push(value);
+                    }
+                  });
+                } else if (typeof meta['@id'] === 'object') {
+                  // Example: { '@id': { '@language': 'en', '@value': 'hand embroidery' } }
+                  values.push(<span>{meta['@id']['@value']}</span>);
+                } else {
+                  // Example: { '@id': 'http://data.silknow.org/collection/4051dfc9-1267-3530-bac8-40011f2e3daa', '@type': 'E78_Collection', label: 'Textiles and Fashion Collection' }
+                  const value = generateValue(query.type, route, metaName, meta);
+                  if (value) {
+                    values.push(value);
+                  }
+                }
+
+                return (
+                  <Metadata key={metaName} label={t(`metadata.${metaName}`)}>
+                    {values.map((value, i) => (
+                      // eslint-disable-next-line react/no-array-index-key
+                      <div key={i}>{value}</div>
+                    ))}
+                  </Metadata>
+                );
+              })}
+            </MetadataList>
+            {/* <Analysis>
               <Tabs>
                 <Tab label="Transcript">
                   <p>Transcript.</p>
@@ -109,81 +260,19 @@ const VideoDetailsPage = ({ result }) => {
                     gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda.
                   </p>
                 </Tab>
-                <Tab label="Face Rec">
-                  <p>Face Rec.</p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                    gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                    gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda.
-                  </p>
-                </Tab>
-                <Tab label="Audio">
-                  <p>Audio.</p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                    gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                    gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda.
-                  </p>
-                </Tab>
-                <Tab label="Object Det">
-                  <p>Object Det.</p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                    gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                    gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda.
-                  </p>
-                </Tab>
               </Tabs>
-            </Analysis>
-          </Primary>
-          <Secondary>
-            <MetadataList>
-              <h1>Une soirée mystérieuse</h1>
-              <Metadata label="Part of">
-                <a href="#">Enquêteur malgré lui</a>
-              </Metadata>
-              <Metadata label="Genre">
-                <a href="#">Série</a>
-              </Metadata>
-              <Metadata label="Themes">
-                <a href="#">Fiction</a>, <a href="#">Intrigue policière</a>
-              </Metadata>
-              <Metadata label="Released">
-                <strong>11 May 2014</strong>
-              </Metadata>
-              <Metadata label="Duration">
-                <strong>45 mins</strong>
-              </Metadata>
-              <Metadata label="Description">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
-                  gravida. Risus commodo viverra maecenas accumsan lacus vel facilisisda
-                </p>
-              </Metadata>
+            </Analysis> */}
+            <Debug>
               <Metadata label="HTTP Parameters">
                 <pre>{JSON.stringify(query, null, 2)}</pre>
               </Metadata>
               <Metadata label="Query Result">
                 <pre>{JSON.stringify(result, null, 2)}</pre>
               </Metadata>
-            </MetadataList>
-            {/* <RelatedVideos>
+            </Debug>
+          </Primary>
+          <Secondary>
+            <div>
               <h2>Related</h2>
               <RelatedVideosList>
                 <Media
@@ -193,7 +282,7 @@ const VideoDetailsPage = ({ result }) => {
                   direction="row"
                 />
               </RelatedVideosList>
-            </RelatedVideos> */}
+            </div>
           </Secondary>
         </Columns>
       </Body>
@@ -204,24 +293,32 @@ const VideoDetailsPage = ({ result }) => {
 
 VideoDetailsPage.getInitialProps = async ({ query }) => {
   const route = config.routes[query.type];
-  const searchQuery = JSON.parse(JSON.stringify(route.query));
-  searchQuery.$limit = 1;
-  searchQuery.$filter = `?id = <${idToUri(query.id, route.uriBase)}>`;
+  const jsonQuery = route.details && route.details.query ? route.details.query : route.query;
+  const searchQuery = JSON.parse(JSON.stringify(jsonQuery));
+  searchQuery.$filter = `?id = <${idToUri(query.id, {
+    base: route.uriBase,
+    encoding: !route.uriBase,
+  })}>`;
 
   try {
-    if (config.debug) {
-      console.log('searchQuery:', JSON.stringify(searchQuery, null, 2));
-    }
-    const res = await sparqlTransformer(searchQuery, {
+    const res = await SparqlClient.query(searchQuery, {
       endpoint: config.api.endpoint,
       debug: config.debug,
     });
-    return { result: res['@graph'][0] };
+    const result = res['@graph'][0];
+    let mediaUrl = null;
+
+    if (result && result.mediaLocator) {
+      // Get media url from the media provider
+      mediaUrl = await locatorToVideo(result.mediaLocator);
+    }
+
+    return { result, mediaUrl };
   } catch (err) {
     console.error(err);
   }
 
-  return { result: null };
+  return { result: null, mediaUrl: null };
 };
 
-export default VideoDetailsPage;
+export default withTranslation('common')(VideoDetailsPage);
