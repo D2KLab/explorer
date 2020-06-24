10199d6560daf61a51833e64dba3fe5f1ec86d0e
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-06-24 09:58:46 +0200

    Fix display of save button dialog

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -40,9 +40,17 @@ const StyledDialogDisclosure = styled(DialogDisclosure)`
 
 const StyledDialogBackdrop = styled(DialogBackdrop)`
   background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
   position: fixed;
-  inset: 0px;
-  z-index: 999;
+  z-index: 2000;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const StyledHeartIcon = styled.span`
