acb1a65fc1066b5e5fccd39d7077326304750cda
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-08-27 15:27:10 +0200

    Add ability to translate site title and description

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 
 import config from '~/config';
+import { withTranslation } from '~/i18n';
 
-const PageTitle = ({ title }) => (
+const PageTitle = ({ title, t }) => (
   <Head>
-    <title>{`${title} | ${config.metadata.title}`}</title>
+    <title>{`${title} | ${t('site.title', config?.metadata?.title)}`}</title>
+    <meta name="description" content={t('site.description', config?.metadata?.description)} />
   </Head>
 );
 
-export default PageTitle;
+export default withTranslation('common')(PageTitle);
