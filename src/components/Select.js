019310b559a51e0c4dfa646a6bdf92bb5f9eac63
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-09-11 15:22:10 +0200

    Add ellipsis to long options values in Select component

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,30 +1,35 @@
 import ReactSelect from 'react-select';
 
 const customStyles = {
-  control: provided => ({
+  control: (provided) => ({
     ...provided,
     border: 'none',
     borderBottom: '2px solid #000',
-    backgroundColor: '#f0f0f0'
+    backgroundColor: '#f0f0f0',
   }),
-  dropdownIndicator: base => ({
+  dropdownIndicator: (base) => ({
     ...base,
     color: 'hsl(0,0%,20%)',
     '&:hover': {
       color: 'hsl(0,0%,20%)',
-    }
-  })
+    },
+  }),
+  option: (base) => ({
+    ...base,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  }),
 };
 
-const customTheme = theme => ({
+const customTheme = (theme) => ({
   ...theme,
   borderRadius: 0,
   colors: {
     ...theme.colors,
     primary: '#000',
     neutral0: '#ccc',
-    primary25: '#bbb'
-  }
+    primary25: '#bbb',
+  },
 });
 
 const Select = ({ ...props }) => (
