b0b5ebb251c90fd22e10180dffb2c90073f1d381
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-09-17 12:04:32 +0200

    SpatioTemporalMaps: add time parameter to iframe src url

--- src/components/SpatioTemporalMaps.js
+++ src/components/SpatioTemporalMaps.js
@@ -3,11 +3,13 @@ import queryString from 'query-string';
 import config from '~/config';
 
 const SpatioTemporalMaps = ({ mapRef, query }) => {
+  const params = { ...query, _t: new Date().getTime() };
+
   return (
     <div>
       <iframe
         title="Spatio Temporal Maps"
-        src={`${config.plugins.spatioTemporalMaps.url}?${queryString.stringify(query)}`}
+        src={`${config.plugins.spatioTemporalMaps.url}?${queryString.stringify(params)}`}
         width="100%"
         height="600"
         ref={mapRef}
