2eb69d1407e64c8738b1697011ea8ab9570173bb
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-11-26 12:15:52 +0100

    Disable camera button since the feature is not available yet

--- src/pages/index.js
+++ src/pages/index.js
@@ -303,6 +303,7 @@ const HomePage = () => {
                     type="button"
                     aria-label={t('common:buttons.searchByImage')}
                     {...dialog}
+                    style={{ opacity: 0.5, pointerEvents: 'none' }}
                   >
                     <CameraIcon />
                   </CameraButton>
