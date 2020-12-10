3308b7a1306a4411a021078b8e1f1c6863417d45
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-12-10 18:26:41 +0100

    Add 404 error page

---
+++ src/pages/404.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import 'intersection-observer';
+
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import Layout from '@components/Layout';
+import Body from '@components/Body';
+import PageTitle from '@components/PageTitle';
+import { useTranslation } from '~/i18n';
+
+const Container = styled.div`
+  padding: 5em 0;
+  width: 100%;
+  text-align: center;
+`;
+
+const NotFoundPage = () => {
+  const { t } = useTranslation('common');
+
+  return (
+    <Layout>
+      <PageTitle title="404 - Uh oh!" />
+      <Header />
+      <Body>
+        <Container>
+          <h1>404 - Uh oh!</h1>
+          <p>{t('errors.pageNotFound')}</p>
+        </Container>
+      </Body>
+      <Footer />
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
