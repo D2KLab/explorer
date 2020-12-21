5b56e5b998404e70c5c17ca696973cf327a83f70
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-12-21 21:22:19 +0100

    Pass current user language to STMaps

--- src/components/SpatioTemporalMaps.js
+++ src/components/SpatioTemporalMaps.js
@@ -2,11 +2,19 @@ import { useMemo } from 'react';
 import queryString from 'query-string';
 import Cookies from 'js-cookie';
 
+import { useTranslation } from '~/i18n';
 import config from '~/config';
 
 const SpatioTemporalMaps = ({ mapRef, query }) => {
+  const { i18n } = useTranslation();
+
   const params = useMemo(() => {
-    return { ...query, _analytics: Cookies.get('consent') === '1', _t: new Date().getTime() };
+    return {
+      ...query,
+      _analytics: Cookies.get('consent') === '1',
+      _t: new Date().getTime(),
+      _l: i18n.language,
+    };
   }, [queryString.stringify(query)]);
 
   return (
