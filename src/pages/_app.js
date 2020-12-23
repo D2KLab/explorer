20a48240b033e7158104d0b22245f6f66140721e
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2020-12-23 11:03:30 +0100

    Convert _app.js to a functional component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth/client';
-import App from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { Provider as ReakitProvider } from 'reakit';
 import { Reset } from 'styled-reset';
@@ -13,45 +12,42 @@ import theme from '~/theme';
 import config from '~/config';
 import { appWithTranslation } from '~/i18n';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    const { session } = pageProps;
+const App = ({ Component, pageProps }) => {
+  const { session } = pageProps;
 
-    return (
-      <ThemeProvider theme={theme}>
-        <ReakitProvider>
-          <Reset />
-          <Head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/images/favicon.png" type="image/png" />
-            <link rel="shortcut icon" href="/images/favicon.png" type="image/png" />
-            {/* Import Lato font */}
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Lato:300,400,700,800"
-            />
-            <link
-              rel="preload"
-              as="style"
-              href="https://fonts.googleapis.com/css?family=Lato:300,400,700,800"
-            />
-          </Head>
-          <NProgress />
-          {config.plugins?.consent?.show && typeof Cookies.get('consent') === 'undefined' ? (
-            <ConsentPopup />
-          ) : (
-            <div />
-          )}
-          {config.plugins?.consent?.show && Cookies.get('consent') === '1' && <RRWebRecorder />}
-          <NextAuth.Provider options={{ site: process.env.SITE }} session={session}>
-            <Component {...pageProps} />
-          </NextAuth.Provider>
-        </ReakitProvider>
-      </ThemeProvider>
-    );
-  }
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <ReakitProvider>
+        <Reset />
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/images/favicon.png" type="image/png" />
+          <link rel="shortcut icon" href="/images/favicon.png" type="image/png" />
+          {/* Import Lato font */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Lato:300,400,700,800"
+          />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css?family=Lato:300,400,700,800"
+          />
+        </Head>
+        <NProgress />
+        {config.plugins?.consent?.show && typeof Cookies.get('consent') === 'undefined' ? (
+          <ConsentPopup />
+        ) : (
+          <div />
+        )}
+        {config.plugins?.consent?.show && Cookies.get('consent') === '1' && <RRWebRecorder />}
+        <NextAuth.Provider options={{ site: process.env.SITE }} session={session}>
+          <Component {...pageProps} />
+        </NextAuth.Provider>
+      </ReakitProvider>
+    </ThemeProvider>
+  );
+};
 
-export default appWithTranslation(MyApp);
+export default appWithTranslation(App);
