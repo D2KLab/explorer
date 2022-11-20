b8b2b9f56b30b3a3188fca7f8b4b86fb31c19df2
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2022-11-20 20:30:32 +0100

    PageTitle: Add project: prefix for translations

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,14 +1,17 @@
 import Head from 'next/head';
-
 import { useTranslation } from 'next-i18next';
+
 import config from '~/config';
 
 function PageTitle({ title }) {
   const { t } = useTranslation('project');
   return (
     <Head>
-      <title>{`${title} | ${t('site.title', config?.metadata?.title)}`}</title>
-      <meta name="description" content={t('site.description', config?.metadata?.description)} />
+      <title>{`${title} | ${t('project:site.title', config?.metadata?.title)}`}</title>
+      <meta
+        name="description"
+        content={t('project:site.description', config?.metadata?.description)}
+      />
     </Head>
   );
 }
