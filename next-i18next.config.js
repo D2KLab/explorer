b05189c11832dbeafcac27d85059ff0d019acbb6
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2023-01-18 14:11:56 +0100

    Set default locale to config.search.defaultLanguage

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -5,7 +5,7 @@ const config = require('./config');
 module.exports = {
   i18n: {
     locales: Object.keys(config.search.languages),
-    defaultLocale: Object.keys(config.search.languages)[0],
+    defaultLocale: config.search.defaultLanguage || Object.keys(config.search.languages)[0],
   },
   localePath: path.resolve('./public/static/locales'),
   detection: {
